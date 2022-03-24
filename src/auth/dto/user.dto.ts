13a809b76e90bd2b689e39c43bbb97162ac52351
SungGil-5125 <sunggil0125@naver.com>
2022-03-24 22:39:41 +0900

    파일 구조 변경

---
+++ src/auth/dto/user.dto.ts
@@ -0,0 +1,11 @@
+import { IsString, IsEmail } from "class-validator";
+export class UserDTO{
+    @IsEmail()
+    email : string
+
+    @IsString()
+    password : string
+
+    @IsString()
+    nickname : string
+}
