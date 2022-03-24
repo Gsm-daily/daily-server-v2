f81362451c8fd518328c8f5844db97033c92349d
SungGil-5125 <sunggil0125@naver.com>
2022-03-24 23:06:41 +0900

    add email

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,27 +1,34 @@
 import { HttpException, HttpStatus, Injectable, UnauthorizedException } from '@nestjs/common';
-import { UserDTO } from 'src/dto/user.dto';
-import { FindOneOptions } from 'typeorm';
-import { AuthRepository } from './auth.repository';
-import { User } from 'src/entity/user.entiy'
+import { UserDTO } from 'src/auth/dto/user.dto';
+import { FindOneOptions, Repository } from 'typeorm';
+import { Auth } from "src/entity/auth.entiy";
 import { InjectRepository } from '@nestjs/typeorm';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { MailerService } from '@nestjs-modules/mailer';
+import { stringify } from 'querystring';
 
 @Injectable()
 export class AuthService {
-    
     constructor(
-    @InjectRepository(AuthRepository)
-    private authRepository : AuthRepository){}
+        @InjectRepository(Auth)
+        private authRepository : Repository<Auth>,  
+        private jwtService : JwtService,
+        private mailerService : MailerService,
+    ){}
 
-    async findbyFeilds(options : FindOneOptions<UserDTO> ) : Promise<User | undefined>{
+    //email, pw, name 찾기
+    async findbyFeilds(options : FindOneOptions<UserDTO> ) : Promise<Auth | undefined>{
         return await this.authRepository.findOne(options);
     }
     
+    //저장
     async save(userDTO : UserDTO) : Promise<UserDTO | undefined> {
         await this.transformPassword(userDTO);
         return await this.authRepository.save(userDTO);
     }
 
+    //bcrypt 변환
     async transformPassword(user : UserDTO) : Promise<void> {
         user.password = await bcrypt.hash(
             user.password, 10,
@@ -29,6 +36,7 @@ export class AuthService {
         return Promise.resolve();
     }
 
+    //user등록
     async registerUser(newUser : UserDTO) : Promise<UserDTO | undefined> {
         const userFind : UserDTO = await this.findbyFeilds({
             where : { email : newUser.email }
@@ -41,16 +49,33 @@ export class AuthService {
         return await this.save(newUser);
     }
 
-    async login(userDTO : UserDTO) : Promise<UserDTO | undefined> {
+    //user login
+    async login(userDTO : UserDTO) : Promise<{Token : string} | undefined> {
         const userFind : UserDTO = await this.findbyFeilds({
             where : { email : userDTO.email }
         })
 
         const comparePassword = await bcrypt.compare(userDTO.password, userFind.password);
+
         if(!userFind || !comparePassword){
-            throw new UnauthorizedException();
+            throw new UnauthorizedException('이메일과 비밀번호를 확인해 주세요.');
         }
 
-        return 
+        const payload = { email : userDTO.email, username : userDTO.nickname}; //sub은 토큰 제목
+
+        return {
+            Token : this.jwtService.sign(payload),
+        };
     }
-}         
+
+    async EmailReceive(userDTO : {email: string}): Promise<void> {
+        console.log('2',userDTO.email);
+            //const number : number = generateRandom(111111, 999999)
+        await this.mailerService.sendMail({
+            to : "jswa7308@naver.com",
+            from : 'sunggil0125@naver.com',
+            subject : 'test',
+            text : '성공해부렸다.'
+        });
+    }
+}          
